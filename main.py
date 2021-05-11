143799272dddf5a015a3dd429e4b3fb3de611838
wukong220 <wusong220@outlook.com>
2021-05-11 17:54:39 +0800

    update

---
+++ main.py
@@ -0,0 +1,4 @@
+ #coding=utf-8
+import random
+menu = ['鸭血粉丝', '黄焖鸡米饭', '虾饺', '秦镇米皮', '麻辣烫', '盖浇饭']
+print(random.choice(menu))
---
+++ test.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+num = 30
+guess = input('猜猜我心里此刻想到的数字：')
+if guess == num:
+	print ('You\'re my Mr. Right!')
+else:
+	print ('You\'re wrong.')
+	
